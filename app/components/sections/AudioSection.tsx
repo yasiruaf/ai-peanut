dd46128ac2cd9c8b5de8a44b97fd3cfeb9ff4366
muhammadyasir7070 <yasir@bigosoft.us>
2024-11-14 19:53:43 +0500

    play and pause icons

--- app/components/sections/AudioSection.tsx
+++ app/components/sections/AudioSection.tsx
@@ -2,16 +2,17 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { IoPauseCircle, IoPlay } from "react-icons/io5";
 
 const AudioSection = () => {
   const items = [
     {
-      imgSrc: "/Images/cat.png",
+      imgSrc: "/Images/cat.png", 
       audioSrc: "/Alvin_Voice/1_You_may_have_killed.mp3",
     },
     {
       imgSrc: "/Images/cat.png",
-      audioSrc: "/Alvin_Voice/2_I_got_Trump_elected_404.mp3",
+      audioSrc: "/Alvin_Voice/2_I_got_Trump_elected_404.mp3", 
     },
     {
       imgSrc: "/Images/cat.png",
@@ -44,24 +45,38 @@ const AudioSection = () => {
   ];
 
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState<number | null>(null);
 
-  const handlePlayAudio = (audioSrc: string) => {
+  const handlePlayAudio = (audioSrc: string, index: number) => {
     if (audio) {
       audio.pause();
+      if (isPlaying === index) {
+        setIsPlaying(null);
+        setAudio(null);
+        return;
+      }
     }
 
     const newAudio = new Audio(audioSrc);
     setAudio(newAudio);
+    setIsPlaying(index);
 
     newAudio.play();
+    
+    newAudio.addEventListener('ended', () => {
+      setIsPlaying(null);
+      setAudio(null);
+    });
   };
 
   return (
     <section className="min-h-screen py-20 bg-[#141414] flex flex-col justify-center items-center border-t-[1px] border-gray-700">
-      <h2 className="text-center text-white font-bold text-5xl mt-12">Click to play Audio</h2>
+      <h2 className="text-center text-white font-bold text-5xl mt-12">
+        Click to play Audio
+      </h2>
       <div className="grid grid-cols-2 md:grid-cols-3">
         {items.map((item, index) => (
-          <div key={index} onClick={() => handlePlayAudio(item.audioSrc)}>
+          <div key={index} className="relative" onClick={() => handlePlayAudio(item.audioSrc, index)}>
             <Image
               src={item.imgSrc}
               alt={`Image ${index + 1}`}
@@ -69,6 +84,13 @@ const AudioSection = () => {
               height={300}
               className="cursor-pointer"
             />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              {isPlaying === index ? (
+                <IoPauseCircle className="text-5xl text-black bg-white rounded-3xl p-2 cursor-pointer" />
+              ) : (
+                <IoPlay className="text-5xl text-black bg-white rounded-3xl p-2 cursor-pointer" />
+              )}
+            </div>
           </div>
         ))}
       </div>
