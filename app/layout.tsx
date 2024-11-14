079f6746bd6561a57a23643ae2964f44161773b2
abdulwahab <abdul.wahab@bigosoft.co.uk>
2024-11-14 20:37:41 +0500

    few fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body
         className={`${font.className}`}
       >
-        {children}
+        {/* {children} */}
       </body>
     </html>
   );
