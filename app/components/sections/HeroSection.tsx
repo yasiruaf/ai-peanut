3b9360be6a4d6fded77c9c2f563d1f6bed4ce48c
abdulwahab <abdul.wahab@bigosoft.co.uk>
2024-11-14 20:00:01 +0500

    small fix

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -9,7 +9,7 @@ export function HomeHeroSection() {
   return (
     <>
       <section className="bg-[#141414] min-h-screen md:px-20 p-8 flex flex-col justify-center items-center">
-        <div className=" aspect-square rounded-2xl bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className=" w-full h-screen md:aspect-square rounded-2xl bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
         <BackgroundBeams />
           <div className="max-w-2xl mx-auto ">
             <div className="mt-12">
