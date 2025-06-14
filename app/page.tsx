296d3ccd9dfbf16e6247162b28fcdf9936423425
yasiruaf <muhammadyasir4741@gmail.com>
2025-06-14 21:01:13 +0500

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -9,40 +9,40 @@ export default function Home() {
       <div className="border-t py-10 flex items-center justify-center border-gray-700 w-full">
         <div className=" p-8  rounded-lg w-full grid md:grid-cols-2 gap-10 container">
           <div>
-            <Image className="aspect-auto rounded-lg shadow-lg" src="/pic_1.png" width={2000} height={2000} alt="aipnut"/>
+            <Image
+              className="aspect-auto rounded-lg shadow-lg"
+              src="/pic_1.png"
+              width={2000}
+              height={2000}
+              alt="aipnut"
+            />
           </div>
           <div className="flex flex-col items-center justify-center">
-          <h2 className="text-4xl md:text-6xl md:text-left mb-10 w-full">About $AiPnut </h2>
-          <p className="text-lg text-center md:text-left">
-          AiPnut is a heartfelt tribute to Peanut the Squirrel, who was taken
-          from his home but whose legacy will never be forgotten. P’Nut’s Family
-          Freedom Farm Animal Sanctuary embodied love, rescuing and caring for
-          injured and abandoned animals. Through the power of AI and blockchain,
-          AiPnut makes Peanut’s spirit immortal.
-          <br />
-          <br />
-          <br />
-          More than a digital memory, AiPnut symbolizes resilience and hope,
-          creating an eternal link to Peanut’s mission. Powered by blockchain,
-          AiPnut preserves Peanut’s memory, transforming him into an enduring
-          symbol of life, freedom, and sanctuary.
-          <br />
-          <br />
-          <br />
-          Peanut forever. AiPnut, eternally.
-          </p>
+            <h2 className="text-4xl md:text-6xl md:text-left mb-10 w-full">
+              About $AiPnut{" "}
+            </h2>
+            <p className="text-lg text-center md:text-left">
+              AiPnut is a heartfelt tribute to Peanut the Squirrel, who was
+              taken from his home but whose legacy will never be forgotten.
+              P’Nut’s Family Freedom Farm Animal Sanctuary embodied love,
+              rescuing and caring for injured and abandoned animals. Through the
+              power of AI and blockchain, AiPnut makes Peanut’s spirit immortal.
+              <br />
+              <br />
+              <br />
+              More than a digital memory, AiPnut symbolizes resilience and hope,
+              creating an eternal link to Peanut’s mission. Powered by
+              blockchain, AiPnut preserves Peanut’s memory, transforming him
+              into an enduring symbol of life, freedom, and sanctuary.
+              <br />
+              <br />
+              <br />
+              Peanut forever. AiPnut, eternally.
+            </p>
           </div>
-         
         </div>
       </div>
       <AudioSection />
-      {/* <div className="border-t min-h-screen relative flex items-center justify-center border-gray-700 w-full">
-       <Image src={"/pic_2.jpeg"} height={2000} width={2000} alt="" className="absolute"/>
-       <div className="">
-
-       </div>
-      </div> */}
-    
       <div className="border-t py-10 flex items-center justify-center border-gray-700 w-full">
         <div className="md:text-2xl text-[10px] p-4 bg-black rounded-lg">
           CA: 6Y6C4Xx7NF2xY5eTaur1ioCmHsAmHDffskJbic2opump
